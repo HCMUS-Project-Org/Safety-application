3e9dd291f302aec8c7b8b65ca8dfb5b74f3d58e2
Nguyen Thanh Quan <quanblue>
2022-06-21 09:32:02 +0700

    fix: logic error on change info

--- changeInfo.py
+++ changeInfo.py
@@ -1,25 +1,37 @@
 import cryptography
 import app
 import authen
+import json
+from flask import session
 
 
 def change_info(new_info, current_info):
-    # change password
-    new_passphase = authen.salt_hash256(new_info["passphase"])
+    if new_info["passphase"] != "":
+        # change password
+        new_passphase = authen.salt_hash256(new_info["passphase"])
 
-    # change Private_key to ensure Integrity
-    origin_priv_key = cryptography.AES_decrypt(
-        current_info["private_key"], current_info["passphase"])
+        # change Private_key to ensure Integrity
+        origin_priv_key = cryptography.AES_decrypt(
+            current_info["private_key"], current_info["passphase"])
 
-    new_priv_key = cryptography.AES_encrypt(origin_priv_key, new_passphase)
+        new_priv_key = cryptography.AES_encrypt(origin_priv_key, new_passphase)
 
-    # update new info
-    new_info["pass"] = new_info["passphase"]
-    new_info["passphase"] = new_passphase
-    new_info["private_key"] = new_priv_key
+        # update new info
+        new_info["pass"] = new_info["passphase"]
+        new_info["passphase"] = new_passphase
+        new_info["private_key"] = new_priv_key
+    else:
+        new_info["passphase"] = current_info["passphase"]
+
+    new_info["avatar"] = current_info["avatar"]
 
     # update user
     app.db.users.update_one(
         {"email": current_info["email"]},
         {"$set": new_info}
     )
+
+    session["user"] = json.dumps(new_info, default=str)
+
+
+# TODO: feature: change avatar
